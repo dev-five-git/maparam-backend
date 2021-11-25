e1e90bb843928f23b5bcc5949b97e5a2451ee3b7
Kang-hadam <xldi29@naver.com>
2021-11-26 00:55:24 +0900

    add/user model

---
+++ v1/models/user.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, String, DateTime, func, Integer, Text
+
+from database import Base
+from database import engine
+
+
+class UserModel(Base):
+    __tablename__ = 'user'
+    id = Column(String(16), primary_key=True)
+    name = Column(String(10))
+    pw = Column(String(64))
+    email = Column(String(50))
+    age = Column(Integer)
+    hashtag = Column(Text)
+    created_at = Column(DateTime, default=func.now())
+
+
+UserModel.__table__.create(bind=engine, checkfirst=True)
