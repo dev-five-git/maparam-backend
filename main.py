9a70290d6265749fd5eb7642206bbc2152ad0129
Kang-hadam <xldi29@naver.com>
2021-11-28 00:19:40 +0900

    add/ dummy for front-test

--- main.py
+++ main.py
@@ -24,4 +24,13 @@ def root():
     return "maparam-api"
 
 
+@app.get("/dummy/list")
+def dummylist():
+    return [{"testkey": "testvalue"}, {"testkey1": "testvalue1"}, {"testkey2": "testvalue2"},
+            {"testkey3": "testvalue3"}, {"testkey4": "testvalue4"}, {"testkey5": "testvalue5"},
+            {"testkey7": "testvalue2"}, {"testkey7": "testvalue7"}, {"testkey8": "testvalue8"},
+            {"testkey9": "testvalue9"}, {"testkey10": "testvalue10"}, {"testkey11": "testvalue11"},
+            {"testkey12": "testvalue12"}]
+
+
 handler = Mangum(app=app)
