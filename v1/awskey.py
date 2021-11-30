e2c45d5f124cb511c58f95d22fc7db811a33f5ae
Kang-hadam <xldi29@naver.com>
2021-12-01 00:26:35 +0900

    set/git ignore

--- v1/awskey.py
+++ v1/awskey.py
@@ -5,3 +5,5 @@ s3 = boto3.client('s3', aws_access_key_id='AKIAYVJ7YJKE5N2FJGML',
                   aws_secret_access_key='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',
                   )
 bucket_name = 'maparam'
+
+#asdasfasgsz
